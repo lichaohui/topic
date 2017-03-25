6c746d176ad97dd48cca3ac9afa6d45d59ab6843
lichaohui <158109640@qq.com>
2017-03-25 13:13:46 +0800

    搭建gulp browsersync服务器

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 gulp.task('default', function() {
   // 将你的默认的任务代码放在这
+});
+
+// 监视文件改动并重新载入
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: './',
+      entry:'app'
+    }
+  });
+
+  gulp.watch(['*.html', 'public/static/css/*.css', 'public/static/js/*.js'], {cwd: 'app'}, reload);
 });
